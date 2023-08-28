e68ead0e3878c717384bd61edb06c63cd4dcb7b2
TaiseiNemoto <nemoto@defide-ix.com>
2023-08-28 10:10:24 +0900

    [WIP]Authの設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+'use client';
+
+import { useSession, signIn, signOut } from 'next-auth/react';
+
 export default function Home() {
+  const { data: session } = useSession();
+
   return (
     <main>
-      <div></div>
+      <div>
+        {!session && (
+          <div>
+            test
+            <button onClick={() => signIn()}>signIn</button>
+          </div>
+        )}
+        {session && (
+          <div>
+            test
+            <button onClick={() => signOut()}>signOut</button>
+          </div>
+        )}
+      </div>
     </main>
   );
 }
